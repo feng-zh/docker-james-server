8609a876ef92e4994280a3be7e7d6d7b40706096
Feng Zhou <feng.zh@gmail.com>
2016-05-04 09:35:06 +0800

    Enable customized library in docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 if [ -e /etc/james-server ]; then
+
   for f in /etc/james-server/*; do
     test -f "$f" && cp $f conf/
   done
+
+  if [ -e /etc/james-server/lib ]; then
+
+    for f in /etc/james-server/lib/*; do
+      test -f "$f" && cp $f conf/lib
+    done
+
+  fi
+
 fi
 
 bin/james start
