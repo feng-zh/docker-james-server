7e83cbfddb8e08add9451e4b670f6d35be4d651d
Feng Zhou <feng.zh@gmail.com>
2016-05-10 18:12:26 +0800

    Optimize build package

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 cd /tmp
 
 mkdir build
 
+mkdir build/m2
+
 cd build
 
 curl -L http://mirrors.ircam.fr/pub/apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz > apache-maven-3.3.1-bin.tar.gz
 
-tar -xvf apache-maven-3.3.1-bin.tar.gz
+tar -xf apache-maven-3.3.1-bin.tar.gz
 
-curl -L https://github.com/apache/james-project/archive/master.tar.gz > master.tar.gz
+curl -L https://github.com/apache/james-project/archive/master.tar.gz > james-project-source.tar.gz
 
-tar -xvf master.tar.gz
+tar -xf james-project-source.tar.gz
 
 cd james-project-*
 
-/tmp/build/apache-maven-3.3.1/bin/mvn package -DskipTests -Pcassandra,elasticsearch,inmemory,exclude-lucene,with-assembly,with-jetm
+export MAVEN_OPTS="-Dmaven.repo.local=/tmp/build/m2"
+
+/tmp/build/apache-maven-3.3.1/bin/mvn --batch-mode package -DskipTests -Pwith-assembly,with-jetm
 
 if [ $? -eq 0 ]; then
    cp server/app/target/james-server-app-*-app.tar.gz /tmp
@@ -37,6 +41,13 @@ cd ${JAMES_HOME}
 rm bin/wrapper-linux-x86-32
 
 sed -i 's/wrapper.daemonize=TRUE/wrapper.daemonize=FALSE/' bin/james
+
+# Fix default package cannot execute
+for name in indexer quota events managesieveserver; do
+    cp conf/${name}-template.xml conf/${name}.xml
+done
+
+rm -rf /tmp/*
 
 rm $0
 
