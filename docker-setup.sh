ce9f2addb75757c3e980fda5ae9ba64584c9c3de
Feng Zhou <feng.zh@gmail.com>
2016-05-10 17:33:41 +0800

    Include build from source code

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,11 +2,33 @@
 
 cd /tmp
 
-curl -L "https://repository.apache.org/service/local/artifact/maven/redirect?r=snapshots&g=org.apache.james&a=james-server-app&v=LATEST&e=tar.gz&c=app" > james-server.tar.gz
+mkdir build
 
-tar xf james-server.tar.gz
+cd build
 
-rm -rf james-server.tar.gz
+curl -L http://mirrors.ircam.fr/pub/apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz > apache-maven-3.3.1-bin.tar.gz
+
+tar -xvf apache-maven-3.3.1-bin.tar.gz
+
+curl -L https://github.com/apache/james-project/archive/master.tar.gz > master.tar.gz
+
+tar -xvf master.tar.gz
+
+cd james-project-*
+
+/tmp/build/apache-maven-3.3.1/bin/mvn package -DskipTests -Pcassandra,elasticsearch,inmemory,exclude-lucene,with-assembly,with-jetm
+
+if [ $? -eq 0 ]; then
+   cp server/app/target/james-server-app-*-app.tar.gz /tmp
+fi
+
+cd /tmp
+
+rm -rf build
+
+tar xf james-server-app-*-app.tar.gz
+
+rm -rf james-server-app-*-app.tar.gz
 
 mv james-server-app*/* ${JAMES_HOME}
 
